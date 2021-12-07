dc71c9864512715c2c1e42f0cacca0ed354c300a
gaertnam <70591046+gaertnam@users.noreply.github.com>
2021-12-06 22:31:29 -0500

    Add files via upload

---
+++ js/display.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+
+//     $(".slidingDiv").hide();
+//     $(".show_hide").show();
+
+//     $('.show_hide').click(function() {
+          
+//          //$(".slidingDiv").slideToggle();
+//          var isvisible = $(this).next('.slidingDiv').is(':visible');
+      
+//          if ( isvisible ) {
+//            $(this).next('.slidingDiv').hide();
+//            console.log("Hide")
+//          } else{
+//            $(this).next('.slidingDiv').show(); 
+//            console.log("Show")
+//          }
+//     });
+
+// });
---
+++ js/ratings.js
@@ -0,0 +1,216 @@
+var acountr = document.getElementById('acountr')
+var acountn = document.getElementById('acountn')
+var bcountr = document.getElementById('bcountr')
+var bcountn = document.getElementById('bcountn')
+var ccountr = document.getElementById('ccountr')
+var ccountn = document.getElementById('ccountn')
+var dcountr = document.getElementById('dcountr')
+var dcountn = document.getElementById('dcountn')
+var ecountr = document.getElementById('ecountr')
+var ecountn = document.getElementById('ecountn')
+var fcountr = document.getElementById('fcountr')
+var fcountn = document.getElementById('fcountn')
+var gcountr = document.getElementById('gcountr')
+var gcountn = document.getElementById('gcountn')
+var hcountr = document.getElementById('hcountr')
+var hcountn = document.getElementById('hcountn')
+var icountr = document.getElementById('icountr')
+var icountn = document.getElementById('icountn')
+var jcountr = document.getElementById('jcountr')
+var jcountn = document.getElementById('jcountn')
+var kcountr = document.getElementById('kcountr')
+var kcountn = document.getElementById('kcountn')
+var lcountr = document.getElementById('lcountr')
+var lcountn = document.getElementById('lcountn')
+
+function countar(){
+    console.log('hello')
+       let cnt = localStorage.getItem('acountr')
+       cnt++
+       acountr.innerHTML = cnt
+      localStorage.setItem('acountr', cnt);
+    }
+
+function countan(){
+    console.log('hello')
+        let cnt = localStorage.getItem('acountn')
+        cnt++
+        acountn.innerHTML = cnt
+        localStorage.setItem('acountn', cnt);
+    }    
+
+function countbr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('bcountr')
+       cnt++
+       bcountr.innerHTML = cnt
+      localStorage.setItem('bcountr', cnt);
+    }
+
+function countbn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('bcountn')
+        cnt++
+        bcountn.innerHTML = cnt
+        localStorage.setItem('bcountn', cnt);
+    }    
+
+function countcr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('ccountr')
+       cnt++
+       ccountr.innerHTML = cnt
+      localStorage.setItem('ccountr', cnt);
+    }
+
+function countcn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('ccountn')
+        cnt++
+        ccountn.innerHTML = cnt
+        localStorage.setItem('ccountn', cnt);
+    }    
+
+function countdr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('dcountr')
+       cnt++
+       dcountr.innerHTML = cnt
+      localStorage.setItem('dcountr', cnt);
+    }
+
+function countdn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('dcountn')
+        cnt++
+        dcountn.innerHTML = cnt
+        localStorage.setItem('dcountn', cnt);
+    }    
+
+function counter(){
+    console.log('hello')
+       let cnt = localStorage.getItem('ecountr')
+       cnt++
+       ecountr.innerHTML = cnt
+      localStorage.setItem('ecountr', cnt);
+    }
+
+function counten(){
+    console.log('hello')
+        let cnt = localStorage.getItem('ecountn')
+        cnt++
+        ecountn.innerHTML = cnt
+        localStorage.setItem('ecountn', cnt);
+    }    
+
+function countfr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('fcountr')
+       cnt++
+       fcountr.innerHTML = cnt
+      localStorage.setItem('fcountr', cnt);
+    }
+
+function countfn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('fcountn')
+        cnt++
+        fcountn.innerHTML = cnt
+        localStorage.setItem('fcountn', cnt);
+    }    
+
+function countgr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('gcountr')
+       cnt++
+       gcountr.innerHTML = cnt
+      localStorage.setItem('gcountr', cnt);
+    }
+
+function countgn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('gcountn')
+        cnt++
+        gcountn.innerHTML = cnt
+        localStorage.setItem('gcountn', cnt);
+    }    
+
+function counthr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('hcountr')
+       cnt++
+       hcountr.innerHTML = cnt
+      localStorage.setItem('hcountr', cnt);
+    }
+
+function counthn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('hcountn')
+        cnt++
+        hcountn.innerHTML = cnt
+        localStorage.setItem('hcountn', cnt);
+    }    
+
+function countir(){
+    console.log('hello')
+       let cnt = localStorage.getItem('icountr')
+       cnt++
+       icountr.innerHTML = cnt
+      localStorage.setItem('icountr', cnt);
+    }
+
+function countin(){
+    console.log('hello')
+        let cnt = localStorage.getItem('icountn')
+        cnt++
+        icountn.innerHTML = cnt
+        localStorage.setItem('icountn', cnt);
+    }    
+
+function countjr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('jcountr')
+       cnt++
+       jcountr.innerHTML = cnt
+      localStorage.setItem('jcountr', cnt);
+    }
+
+function countjn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('jcountn')
+        cnt++
+        jcountn.innerHTML = cnt
+        localStorage.setItem('jcountn', cnt);
+    }    
+
+function countkr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('kcountr')
+       cnt++
+       kcountr.innerHTML = cnt
+      localStorage.setItem('kcountr', cnt);
+    }
+
+function countkn(){
+    console.log('hello')
+        let cnt = localStorage.getItem('kcountn')
+        cnt++
+        kcountn.innerHTML = cnt
+        localStorage.setItem('kcountn', cnt);
+    }    
+
+function countlr(){
+    console.log('hello')
+       let cnt = localStorage.getItem('lcountr')
+       cnt++
+       lcountr.innerHTML = cnt
+      localStorage.setItem('lcountr', cnt);
+    }
+
+function countln(){
+    console.log('hello')
+        let cnt = localStorage.getItem('lcountn')
+        cnt++
+        lcountn.innerHTML = cnt
+        localStorage.setItem('lcountn', cnt);
+    }    
